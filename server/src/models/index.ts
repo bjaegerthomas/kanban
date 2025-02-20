6270c5001630eace8c616344999b9d536edf60e7
jaegerthomas <bjaegerthomas@gmail.com>
2025-02-20 09:57:06 -0500

    updated index.ts for sequelize to remove type any

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -11,9 +11,9 @@ if (process.env.DB_URL) {
   sequelize = new Sequelize(process.env.DB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
+    process.env.DB_NAME || 'default_db_name',
+    process.env.DB_USER || 'default_user',
+    process.env.DB_PW || 'default_password',
     {
       host: 'localhost',
       dialect: 'postgres',
